16241ef36a7e48e5e4166987aae5d2ea491d4379
gochujjang <41176663+gochujjang@users.noreply.github.com>
2024-01-05 09:23:07 +0700

    api porto

--- lib/services/porto_service.dart
+++ lib/services/porto_service.dart
@@ -7,22 +7,26 @@ class PortoService {
   tambah({
     required String status,
     required int nominal,
-    required String 
+    required String porto,
   }) async {
+    String? token = box.read("token");
     try {
       var response = await Dio().post(
         "https://reqres.in/api/users",
         options: Options(
           headers: {
             "Content-Type": "application/json",
+            "Authorization": "Bearer $token",
           },
         ),
         data: {
-          "name": "morpheus",
-          "job": "programmer",
+          "status": status,
+          "nominal": nominal,
+          "porto": porto,
         },
       );
       Map obj = response.data;
+      print("Response OBJ = ${obj}");
     } on Exception catch (err) {
       print(err);
     }
